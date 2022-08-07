e244bc121c1e162a8184902528849ad2de6c7774
Jorge Galat <jrgglt@gmail.com>
2022-08-07 20:17:39 -0300

    asd

--- packages/app/constants/colors.ts
+++ packages/app/constants/colors.ts
@@ -3,14 +3,14 @@ export default {
     text: "#000",
     background: "#fff",
     primary: "#e60070",
-    iconDefault: "#999",
+    icon: "#999",
     placeholder: "#999"
   },
   dark: {
     text: "#fff",
     background: "#000",
     primary: "#e60070",
-    iconDefault: "#ccc",
+    icon: "#ccc",
     placeholder: "#ccc",
   },
 };
