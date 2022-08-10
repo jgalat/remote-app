c06fc99a511905360ddb924822c81972301da3db
Jorge Galat <jrgglt@gmail.com>
2022-08-10 15:54:48 -0300

    theme

--- packages/app/hooks/use-theme-color.ts
+++ packages/app/hooks/use-theme-color.ts
@@ -1,12 +1,13 @@
 import Colors from "../constants/colors";
 import { useColorScheme } from "./use-settings";
 
-export default function useThemeColor(
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
-  return Colors[useColorScheme()][colorName];
-}
-
 export function useTheme() {
   return Colors[useColorScheme()];
 }
+
+export default function useThemeColor(
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+  return useTheme()[colorName];
+}
+
