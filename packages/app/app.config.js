5a48cacbbd696fa990c587c13b496cb6328c26e2
Jorge Galat <jrgglt@gmail.com>
2025-06-18 17:10:25 -0400

    chore: use 36

--- packages/app/app.config.js
+++ packages/app/app.config.js
@@ -69,9 +69,9 @@ export default {
         {
           android: {
             usesCleartextTraffic: true,
-            compileSdkVersion: 35,
-            targetSdkVersion: 35,
-            buildToolsVersion: "35.0.0",
+            compileSdkVersion: 36,
+            targetSdkVersion: 36,
+            buildToolsVersion: "36.0.0",
           },
         },
       ],
