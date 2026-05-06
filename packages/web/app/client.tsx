8504898bc74318152570164c51fe21dc67bc722b
Jorge Galat <jrgglt@gmail.com>
2026-05-05 22:31:40 -0300

    chore: drop packages/web

--- packages/web/app/client.tsx
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-import { hydrateRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start/client";
-
-hydrateRoot(document, <StartClient />);
